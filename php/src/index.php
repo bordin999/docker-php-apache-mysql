abc04d7543c55a2cf8fa428b88b9716521ad78b4
bordin999 <bordin999@gmail.com>
2023-08-21 14:24:22 +0700

    my docker

---
+++ php/src/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>123</title>
+</head>
+
+<body>
+    <h1>Hello My Docker</h1>
+    <p>We have</p>
+    <ul>
+        <li>PHP port : 8080</li>
+        <li>Apache</li>
+        <li>MYSQL port : 8000</a></li>
+    </ul>
+</body>
+
+</html>
